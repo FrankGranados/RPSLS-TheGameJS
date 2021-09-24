4fac2de373a54a48ec8851c636338fcc98c93c4d
FrankGranados <frank.granados86@gmail.com>
2021-09-24 02:33:37 -0500

    notas 1.05

--- script.js
+++ script.js
@@ -1,66 +1,78 @@
-const   boxResult = document.getElementById("result"),
-        humanSelect = document.getElementById("humanSelect"),
-        machineSelect = document.getElementById("machineSelect"),
-        human_hp = document.getElementById("human_hp"),
-        machine_hp = document.getElementById("machine_hp")
-        options = ["Piedra", "Papel", "Tijera", "Spock", "Lagarto"];
+const   boxResult = document.getElementById("boxResult"),
+        human_Health = document.getElementById("human_Health"),
+        machine_Health = document.getElementById("machine_Health"),
+        options = ["Piedra", "Papel", "Tijera", "Lagarto", "Spock"];
 
+// CHOOSING A PLAY
 function play(humanMove) {
     var machineMove = machine_move(),
         result = gameResult(humanMove,machineMove);
-        
-        
-      
-    if (human_hp.textContent <= "0") {
+    
+    //IF HEALTH POINTS IS 0, PLAY ENDS
+    if (human_Health.textContent <= "0") {
         boxResult.innerHTML = "SE ACABO, ENTIENDELO ヽ(ಠ_ಠ)ノ"
         return
-    } else if (machine_hp.textContent <= "0") {
+    } else if (machine_Health.textContent <= "0") {
         boxResult.innerHTML = "APLASTASTE A TU ENEMIGO (•̀o•́)ง"
         return
     }
     
-    
-    humanSelect.innerHTML = options[humanMove];
-    machineSelect.innerHTML = options[machineMove];
+    //PRINTING PLAY IMAGES
+    document.getElementById("human_img").src = "./images/option" + humanMove +".png";
+    document.getElementById("machine_img").src = "./images/option" + machineMove +".png";
 
+    //PRINTING PLAY MARKER
+    document.getElementById("humanSelect").innerHTML = options[humanMove];
+    document.getElementById("machineSelect").innerHTML = options[machineMove];
+    
+
+
+    // GAME RESULT
     switch (result) {
         case 0:
             boxResult.innerHTML = "Ganaste";
-            machine_hp.textContent -= 1
+            machine_Health.textContent -= 1
             break;
         case 1:
             boxResult.innerHTML = "Empate";
             break;
         case 2:
             boxResult.innerHTML = "Perdiste";
-            human_hp.textContent -= 1
+            human_Health.textContent -= 1
             break;
         default:
             break;
     }    
 }
 
-function machine_move() {   //SELECCIONA UN NUMERO ALEATORIO DEL 0 AL 4
+// RANDOM MACHINE PLAY
+function machine_move() { 
     return Math.floor(Math.random() * 5);
 }
 
-function gameResult(humanMove,machineMove) { //Funcion para definir el resultado del juego
-    var result= humanMove == 0 && machineMove == 2 ||   
-                humanMove == 0 && machineMove == 4 ||   
-                humanMove == 1 && machineMove == 0 ||   
-                humanMove == 1 && machineMove == 3 ||   //Se enlistan todas las condiciones de victoria
-                humanMove == 2 && machineMove == 1 ||   
-                humanMove == 2 && machineMove == 4 ||   
-                humanMove == 3 && machineMove == 2 ||   
-                humanMove == 3 && machineMove == 0 ||   
-                humanMove == 4 && machineMove == 1 ||   
-                humanMove == 4 && machineMove == 3  
+// VICTORY CONDITIONS
+function gameResult(humanMove,machineMove) { 
+    var result= humanMove == 0 && machineMove == 3 ||
+                humanMove == 0 && machineMove == 2 ||
+                humanMove == 1 && machineMove == 0 ||
+                humanMove == 1 && machineMove == 4 ||
+                humanMove == 2 && machineMove == 1 ||
+                humanMove == 2 && machineMove == 3 ||
+                humanMove == 3 && machineMove == 1 ||
+                humanMove == 3 && machineMove == 4 ||
+                humanMove == 4 && machineMove == 0 ||
+                humanMove == 4 && machineMove == 2  
                 ? 0 : humanMove == machineMove ? 1 : 2
     return result
 }
 
-function reboot() {
-    human_hp.textContent = 4
-    machine_hp.textContent = 4
-    boxResult.innerHTML = "comenzemos otra vez ＼(｀0´)／"
+// RESTART GAME
+function reStart() {
+    human_Health.textContent = 4
+    machine_Health.textContent = 4
+    boxResult.innerHTML = "Otra Vez ＼(｀0´)／"
+
+    //PRINTING PLAY IMAGES
+    document.getElementById("human_img").src = "./images/question.png";
+    document.getElementById("machine_img").src = "./images/question.png";
 }
